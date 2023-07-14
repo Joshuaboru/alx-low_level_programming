71e36f7e5fcdfda3ea2f75966be1a3bc8387c64b
Joshuaboru <borujoshua0@gmail.com>
2023-07-14 07:48:41 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,31 +6,25 @@
 
 int main(void)
 {
-int i, j;
 
-for (i = 0; i < 100; i++)
+int p, q;
+
+for (p = 0; p <= 98; p++)
 {
-for (j = i; j < 100; j++)
+
+for (q = p + 1; q <= 99; q++)
 {
-int first_digit_i = i / 10;
-int second_digit_i = i % 10;
-int first_digit_j = j / 10;
-int second_digit_j = j % 10;
-putchar(first_digit_i / 10 + '0');
-putchar(first_digit_i % 10 + '0');
-putchar(second_digit_i / 10 + '0');
-putchar(second_digit_i % 10 + '0');
+putchar((p / 10) + '0');
+putchar((p % 10) + '0');
 putchar(' ');
-putchar(first_digit_j / 10 + '0');
-putchar(first_digit_j % 10 + '0');
-putchar(second_digit_j / 10 + '0');
-putchar(second_digit_j % 10 + '0');
-if (!(i == 99 && j == 99))
-{
+putchar((q / 10) + '0');
+putchar((q % 10) + '0');
+if (p == 98 && q == 99)
+continue;
 putchar(',');
 putchar(' ');
 }
 }
-}
+putchar('\n');
 return (0);
 }
