6d081f1c3054362ee791fa92ed7bf1fd1e2eb793
Joshuaboru <borujoshua0@gmail.com>
2023-07-18 06:57:44 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+int i;
+unsigned int a = 1, b = 2, c;
+printf("%u, %u", a, b);
+for (i = 2; i < 98; i++)
+{
+c = a + b;
+printf(", %u", c);
+a = b;
+b = c;
+}
+printf("\n");
+return (0);
+}
