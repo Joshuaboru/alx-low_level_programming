36af86d6b3ef5ee5e58fd560c86f914ce0ae98a9
Joshuaboru <borujoshua0@gmail.com>
2023-08-09 09:44:49 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,60 +1,71 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "main.h"
 
 /**
- * strtow - splits a string into words.
- * @str: string to split.
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
  *
- * Return: pointer to an array of strings (words).
- * Each element of this array should contain a single word,
- * null-terminated.
- * The last element of the returned array should be NULL.
- * Words are separated by spaces.
- * Returns NULL if str == NULL or str == "".
- * If your function fails, it should return NULL.
+ * Return: number of words
+ */
+
+int count_word(char *s)
+{
+int flag, c, w;
+flag = 0;
+w = 0;
+for (c = 0; s[c] != '\0'; c++)
+{
+if (s[c] == ' ')
+flag = 0;
+else if (flag == 0)
+{
+flag = 1;
+w++;
+}
+}
+return (w);
+}
+
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
  */
 char **strtow(char *str)
 {
-char **words = NULL;
-int i, j, k, len, count;
-
-if (str == NULL || *str == '\0')
-return (NULL);
-
-count = 0;
-for (i = 0; str[i] != '\0'; i++)
-{
-if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-count++;
-}
-
-words = malloc(sizeof(char *) * (count + 1));
-if (words == NULL)
-return (NULL);
-
-for (i = 0, j = 0; str[i] != '\0'; i++)
-{
-if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-{
-len = 0;
-for (k = i; str[k] != '\0' && str[k] != ' '; k++)
+char **matrix, *tmp;
+int i, k = 0, len = 0, words, c = 0, start, end;
+while (*(str + len))
 len++;
-
-words[j] = malloc(sizeof(char) * (len + 1));
-if (words[j] == NULL)
-{
-for (k = 0; k < j; k++)
-free(words[k]);
-free(words);
+words = count_word(str);
+if (words == 0)
 return (NULL);
+matrix = (char **) malloc(sizeof(char *) * (words + 1));
+if (matrix == NULL)
+return (NULL);
+for (i = 0; i <= len; i++)
+{
+if (str[i] == ' ' || str[i] == '\0')
+{
+if (c)
+{
+end = i;
+tmp = (char *) malloc(sizeof(char) * (c + 1));
+if (tmp == NULL)
+return (NULL);
+while (start < end)
+*tmp++ = str[start++];
+*tmp = '\0';
+matrix[k] = tmp - c;
+k++;
+c = 0;
 }
-strncpy(words[j], &str[i], len);
-words[j][len] = '\0';
-j++;
 }
+else if (c++ == 0)
+start = i;
 }
-words[j] = NULL;
-return (words);
+matrix[k] = NULL;
+return (matrix);
 }
