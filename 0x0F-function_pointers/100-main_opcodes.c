13ff10bd90b6b3a4803e97cbbc9c9160ee27cd19
Joshuaboru <borujoshua0@gmail.com>
2023-08-16 05:37:39 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "function_pointers.h"
+
 /**
-* main - entry point to func
-*
-* Description: program that prints the opcodes of it own main function.
-* Return: Always 0
-*/
-int main(void)
+ * main - prints the opcodes of its own main function.
+ * @argc: number of arguments passed to the function.
+ * @argv: array of pointers to the arguments.
+ *
+ * Return: 0 on success, 1 on incorrect number of arguments,
+ * 2 if number of bytes is negative.
+ */
+int main(int argc, char **argv)
 {
-	return (0);
+int i, bytes;
+
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+bytes = atoi(argv[1]);
+if (bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+for (i = 0; i < bytes; i++)
+printf("%02x%c", *((unsigned char *)main + i), i == bytes - 1 ? '\n' : ' ');
+return (0);
 }
